29f20a553bad474474adf1c8582d2793d82c38e6
Ubuntu <ubuntu@ip-172-31-34-218.us-east-2.compute.internal>
2019-10-22 22:32:47 +0000

    adjusted neural network model

--- neural_network/meteo_nn.js
+++ neural_network/meteo_nn.js
@@ -8,6 +8,17 @@ var inputData = [];
 var classificationData = [];
 var model;
 
+/* model example of zone
+var zoneInfo = {
+    latitude : 5,
+    longitude : 16,
+    precipitation : 2,
+    wind : 1,
+    danger : 0 <- means no alert, 1 means alert
+};
+In the data.txt represented as 5,16,2,1,0
+*/
+
 //tf.loadLayersModel returns a promise so it needs a then and a catch in to order to wait until it resolves
 loadedModel = tf.loadLayersModel("model.json");
 loadedModel.then(function(result){
@@ -18,43 +29,36 @@ loadedModel.then(function(result){
 }).catch(function(error) {
 	//In case model does not exist, create it defining its properties (only for first time execution)
 	model = tf.sequential();
-	model.add(tf.layers.dense({units: 10, activation: 'sigmoid',inputShape: [4]}));
-	model.add(tf.layers.dense({units: 4, activation: 'sigmoid',inputShape: [10]}));
+	model.add(tf.layers.dense({units: 4, activation: 'sigmoid',inputShape: [4]}));
+	model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
 	model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
 	train();
 });
 
-train(); // TODO remove this
+train();
 
 async function train() {
 
 	await readData();
 	
-	//This commented lines are the structure of arrays compatibles with the neural network model used
-	//const training_data = tf.tensor2d([[0],[1],[2],[3],[4]]);
-	//const target_data = tf.tensor2d([[0,0,0],[0,0,1],[0,1,0],[1,0,0],[1,0,0]]);
-	
 	const training_data = tf.tensor2d(inputData);
 	const target_data = tf.tensor2d(classificationData);
 
-	for (let i = 1; i < 400; ++i) {
+	for (let i = 1; i < 10; ++i) {
 		var h = await model.fit(training_data, target_data, {epochs: 30});
 		console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
 	}
-	//After every training, persist the model with its weigths
-	//model.save("file:///home/ubuntu/nn_model");
-	predictValuesAndPersist();
-	log('\n' + new Date() + ' model training completed.');
 }
 
 async function readData(){
+	var dataPath ='./meteo_data/data.txt';
 	return new Promise((resolve) => {
-		lineReader.eachLine('./nn_data/data.txt', function(line_, last) {
+		lineReader.eachLine(dataPath, function(line_, last) {
 			//Fist number is the input
 			line = line_.split(",");
 			line = line.map(number => number = parseInt(number,10)); //Convert from char to number
 			inputData.push([line[0],line[1],line[2],line[3]]);
-			classificationRow = line[5];
+			classificationRow = [line[4]];
 			classificationData.push(classificationRow);
 			if(last){
 				resolve(); //method called when promess executes succesfully
@@ -63,43 +67,14 @@ async function readData(){
 	});
 }
 
-/*
-async function predictValuesAndPersist(){
-	path = '/home/ubuntu/meanderlust-rest/nn_data/prediction.txt';
-	fs.unlinkSync(path); //clean file
-	predictionValues = [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23]];
-	result = model.predict(tf.tensor2d(predictionValues));
-	result.array().then(array => {
-		array.map((row,index) => {
-			fs.appendFile(path,index + '-' + genres[getIndexOfMaxValue(row)] + '\n',function(){
-			});
-			log('\n' + index + '-' + genres[getIndexOfMaxValue(row)]);
-		});
-	});
-}
-*/
-
 async function predictValues(zoneInfo){
+	path = '/home/ubuntu/meteo-prediction/nn_data/meteo_prediction.txt';
 	predictionValues = [[zoneInfo.latitude, zoneInfo.longitude, zoneInfo.precipitation, zoneInfo.wind]];
 	result = model.predict(tf.tensor2d(predictionValues));
 	result.array().then(array => {
 		array.map((row,index) => {
-			fs.appendFile(path,index + '-' + row) + '\n',function(){
-				console.log(path, index + '-' + row);
+			fs.appendFile(path,row) + '\n',function(){
 			};
 		});
 	});
 }
-
-function log(message){
-	fs.appendFile('/home/ubuntu/neural_network.log',message, (err) => {
-	});
-}
-
-var zoneInfo = {
-    latitude : 0,
-    longitude : 0,
-    precipitation : 0,
-    wind : 0,
-    danger : 0
-};
